d230d16bddf2e20fb2f597b798ca33803ca7a7cb
hallefy <hallefy@fretebras.com.br>
2017-08-28 12:15:11 -0300

    View Detail - Showing Image and movie description

--- app/src/main/java/com/example/hallefy/filmskotlin/views/moviedetail/DetailPresenter.kt
+++ app/src/main/java/com/example/hallefy/filmskotlin/views/moviedetail/DetailPresenter.kt
@@ -1,17 +1,15 @@
 package com.example.hallefy.filmskotlin.views.moviedetail
 
 import android.app.Activity
-import android.media.Image
-import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.hallefy.filmskotlin.NetworkAPI.models.Movie
 import com.example.hallefy.filmskotlin.R
 import com.example.hallefy.filmskotlin.views.movielist.utils.loadUrl
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
-import kotlinx.android.synthetic.main.activity_detalhes.*
 import javax.inject.Inject
 
 /**
@@ -22,22 +20,24 @@ class DetailPresenter @Inject constructor(private val interactor: DetailMVP.Inte
 
     var description : TextView? = null
     var imageFilm : ImageView? = null
+    var release : TextView? = null
+    var progressBar : ProgressBar? = null
+
 
     override fun onNext(response : Movie?) {
-        initComponents()
-        println(response!!.overview)
-        description!!.text = response!!.overview
-        imageFilm!!.loadUrl("https://image.tmdb.org/t/p/w500/"+response!!.posterPath)
+        progressBar!!.visibility = View.GONE
+
+        description!!.text = "Description: " + response!!.overview
+        imageFilm!!.loadUrl("https://image.tmdb.org/t/p/w500/"+response!!.backdropPath)
+        release!!.text = "Release data:" + response!!.releaseDate
     }
 
     override fun onSubscribe(d: Disposable?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        println("TESTE.OnSubscribe")
     }
 
     override fun onComplete() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        println("TESTE.onComplete")
 
     }
 
@@ -47,15 +47,23 @@ class DetailPresenter @Inject constructor(private val interactor: DetailMVP.Inte
 
 
     override fun requestDetail(id_movie : Int) {
+        initComponents()
+
+        progressBar!!.visibility = View.VISIBLE
+        progressBar!!.requestFocus()
+
         var interactor = DetailInteractor()
         interactor.getMovieDetail(id_movie, this)
     }
 
     fun initComponents(){
 
-
+        release = activity.findViewById(R.id.tvRelease) as TextView
         description = activity.findViewById(R.id.tvDescriptionFilm) as TextView
         imageFilm = activity.findViewById(R.id.imgBannerFilm) as ImageView
+        progressBar = activity.findViewById(R.id.progressBarDetail) as ProgressBar
+
+
     }
 
 
